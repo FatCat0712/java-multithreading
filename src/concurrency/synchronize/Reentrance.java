fef63fd3d83b4e6543aa50f3af2484901a24fc85
Fatcat0712 <maisonhai456@gmail.com>
2026-02-11 13:59:47 +0700

    java synchronized

---
+++ src/concurrency/synchronize/Reentrance.java
@@ -0,0 +1,14 @@
+package concurrency.synchronize;
+
+public class Reentrance {
+    private int count = 0;
+
+    public synchronized void inc() {
+        this.count++;
+    }
+
+    public synchronized int incAndGet() {
+        inc();
+        return this.count;
+    }
+}
