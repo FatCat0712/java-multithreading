05554086f47403faa7bca5da42b1e55e2b5f65f9
Fatcat0712 <maisonhai456@gmail.com>
2026-02-13 18:59:56 +0700

    race conditions

---
+++ src/concurrency/race_conditions/RaceConditionsExample2.java
@@ -0,0 +1,24 @@
+package concurrency.race_conditions;
+
+public class RaceConditionsExample2 {
+    public static void main(String[] args) {
+        CounterSynchronized counter = new CounterSynchronized();
+
+        Thread thread1 = new Thread(getRunnable(counter, "Thread1 final count: "));
+        Thread thread2 = new Thread(getRunnable(counter, "Thread2 final count: "));
+
+        thread1.start();
+        thread2.start();
+
+    }
+
+    private static Runnable getRunnable(CounterSynchronized counter, String message) {
+        return () -> {
+            for(int i = 0; i < 1_000_000; i++) {
+                counter.incAndGet();
+            }
+
+            System.out.println(message + counter.get());
+        };
+    }
+}
